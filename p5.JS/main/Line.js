fdc8d8184eb982545f45a578ef59cc6da678b3bf
Bruno Martins Gomes de Santana <bmgs@ug3c19.windows.cin.ufpe.br>
2019-11-22 19:13:46 -0300

    Line feito?

---
+++ p5.JS/main/Line.js
@@ -0,0 +1,16 @@
+class Line{
+     var p1;
+     var p2;
+
+    Line(pp1,pp2){
+        p1 = new Point(pp1);
+        p2 = new Point(pp2);
+    }
+
+    void show{
+        ctx.moveTo(p1.x,p1.y)
+        fill(0);
+        ctx.lineTo(p2.x,p2.y);
+        ctx.stroke();
+    }
+}
