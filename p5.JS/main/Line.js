4da78d248fbe2bf758913a4ea638aff6e6113a5b
Jose Gabriel Silva Pereira <jgsp2@ug3c17.windows.cin.ufpe.br>
2019-11-22 19:51:54 -0300

    line ajeitado

--- p5.JS/main/Line.js
+++ p5.JS/main/Line.js
@@ -1,16 +1,13 @@
 class Line{
-     var p1;
-     var p2;
+	constructor(pp1,pp2){
+		this.p1 = new Point(pp1);
+		this.p2 = new Point(pp2);
+	}
 
-    Line(pp1,pp2){
-        p1 = new Point(pp1);
-        p2 = new Point(pp2);
-    }
-
-    function show{
-        ctx.moveTo(p1.x,p1.y)
-        fill(0);
-        ctx.lineTo(p2.x,p2.y);
-        ctx.stroke();
-    }
+	show{
+		ctx.moveTo(p1.x,p1.y);
+		fill(0);
+		ctx.lineTo(p2.x,p2.y);
+		ctx.stroke();
+	}
 }
